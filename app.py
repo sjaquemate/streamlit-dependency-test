2cda5702a73f11b857d64818a2cc3906519b0bac
Teun Schaeken <teun.schaeken@gmail.com>
2022-09-14 14:00:55 +0200

    matplotlib without dependency

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import subprocess
 output = subprocess.check_output(["python", "--version"]).decode()
 st.write(output)
     
-
 with st.spinner("Installing pandas"):
     output = subprocess.check_output(["python", "-m", "pip", "install", "pandas"]).decode()
+    # output = subprocess.check_output(["python", "-m", "pip", "install", "pandas"]).decode()
     st.write(output)
     
 import pandas as pd 
+import matplotlib.pyplot as plt 
 
 st.write(pd.__version__)
+fig, ax = plt.subplots()
+ax.plot(range(10))
+st.pyplot(fig)
