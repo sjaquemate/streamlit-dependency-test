8bbb7384cfd05b54a77132f634ba7c12962dace0
Teun Schaeken <teun.schaeken@gmail.com>
2022-09-14 14:22:34 +0200

    upd

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ outputs = load_dependencies()
 st.write ( subprocess.check_output(["python", "-m", "pip", "list"]).decode() )
 st.write(outputs)
 
-import matplotlib 
-st.write(matplotlib.__version__)
-
 subprocess.check_output("python dep_write.py")
 
 with open('file.txt') as f:
