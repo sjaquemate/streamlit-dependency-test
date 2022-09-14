8900ff427cf53546b6699a265ae62db6110ed41e
Teun Schaeken <teun.schaeken@gmail.com>
2022-09-14 14:31:27 +0200

    upd

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ outputs = load_dependencies()
 st.write ( subprocess.check_output(["python", "-m", "pip", "list"]).decode() )
 st.write(outputs)
 
-st.write( subprocess.check_output('python -c "import os; print(os.listdir())"').decode() ) 
+st.write( subprocess.check_output(["python", "-c" ,"import os; print(os.listdir())"]).decode() ) 
 st.write( subprocess.check_output("ls").decode() )
-subprocess.check_output("python dep_write.py")
+subprocess.check_output(["python", "dep_write.py"])
 
 with open('file.txt') as f:
     lines = f.readlines()
