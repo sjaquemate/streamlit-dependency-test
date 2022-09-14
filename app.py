0f0fda9a05020c2c04f379e1bbeb6f74db8500f6
Teun Schaeken <teun.schaeken@gmail.com>
2022-09-14 14:03:55 +0200

    upd

--- app.py
+++ app.py
@@ -3,16 +3,23 @@ import subprocess
 
 output = subprocess.check_output(["python", "--version"]).decode()
 st.write(output)
+
+@st.cache 
+def load_dependencies():
+    with st.spinner("Installing pandas"):
+        output = subprocess.check_output(["python", "-m", "pip", "install", "pandas"]).decode()
+        output = subprocess.check_output(["python", "-m", "pip", "install", "matplotlib"]).decode()
+        st.write(output)
     
-with st.spinner("Installing pandas"):
-    output = subprocess.check_output(["python", "-m", "pip", "install", "pandas"]).decode()
-    output = subprocess.check_output(["python", "-m", "pip", "install", "matplotlib"]).decode()
-    st.write(output)
-    
+load_dependencies()
+
 import pandas as pd 
+import matplotlib 
+st.write(pd.__version__)
+st.write(matplotlib.__version__)
+
 import matplotlib.pyplot as plt 
 
-st.write(pd.__version__)
 fig, ax = plt.subplots()
 ax.plot(range(10))
 st.pyplot(fig)
